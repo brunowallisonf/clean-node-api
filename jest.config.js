8a89e8dcb86e3d720bc1ba169be5af922eca7d49
Bruno Wallison <brunowallison.fernandes@gmail.com>
2022-03-14 20:38:31 -0300

    chore: add jest configs

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+export default {
+  roots: ['<rootDir>/src/'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  collectCoverageFrom: '<rootDir>/src/**/*.ts',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
