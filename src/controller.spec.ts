5c740b6d011ca9c75ba6528b3dd0d82942cbdd45
Bruno Wallison <brunowallison.fernandes@gmail.com>
2022-03-14 22:02:37 -0300

    feat: ensure signup controller return 400 if no name is provided

--- src/controller.spec.ts
+++
@@ -1,5 +0,0 @@
-describe('Test 1', () => {
-  test('Teste', () => {
-    expect(1).toBe(1)
-  })
-})
---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,16 @@
+import { SignUpController } from './SignUp'
+describe('Signup controller', () => {
+  test('Should return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpRespose = sut.handle(httpRequest)
+    expect(httpRespose.statusCode).toBe(400)
+  })
+})
