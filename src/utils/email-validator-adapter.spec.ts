0288459ea9fd680fa20b409e854e3757bc9738e8
Bruno Wallison <brunowallison.fernandes@gmail.com>
2022-03-31 23:28:18 -0300

    test: add error case

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,10 +1,30 @@
 import { EmailValidatorAdapter } from './email-validator'
+import validator from 'validator'
 
+jest.mock('validator', () => ({
+  isEmail (): Boolean {
+    return true
+  }
+}))
 describe('EmailValidator adapter', () => {
   test('should return false if validator returns false', () => {
     const sut = new EmailValidatorAdapter()
-    const isValid = sut.isValid('invalid_email@mail.com')
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+    const isValid = sut.isValid('invalid_email@mail..com')
 
     expect(isValid).toBe(false)
+  })
+  test('should return true if validator returns true', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('valid@mail.com')
+
+    expect(isValid).toBe(true)
+  })
+  test('should call validator with correct email', () => {
+    const sut = new EmailValidatorAdapter()
+    const spy = jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+    sut.isValid('valid@mail.com')
+
+    expect(spy).toBeCalledWith('valid@mail.com')
   })
 })
