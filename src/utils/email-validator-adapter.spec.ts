52799306d6288805eb3537a8f017a99a56d40b57
Bruno Wallison <brunowallison.fernandes@gmail.com>
2022-04-04 08:02:44 -0300

    feat: add email validator and encrypter protocols

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,4 +1,4 @@
-import { EmailValidatorAdapter } from './email-validator'
+import { EmailValidatorAdapter } from './email-validator-adapter'
 import validator from 'validator'
 
 jest.mock('validator', () => ({
@@ -6,22 +6,27 @@ jest.mock('validator', () => ({
     return true
   }
 }))
+
+const makeSut = (): EmailValidatorAdapter => {
+  const sut = new EmailValidatorAdapter()
+  return sut
+}
 describe('EmailValidator adapter', () => {
   test('should return false if validator returns false', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_email@mail..com')
 
     expect(isValid).toBe(false)
   })
   test('should return true if validator returns true', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     const isValid = sut.isValid('valid@mail.com')
 
     expect(isValid).toBe(true)
   })
   test('should call validator with correct email', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     const spy = jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     sut.isValid('valid@mail.com')
 
