b1ba32532e33d69a21b2c25e3e1db8e428e71120
Bruno Wallison <brunowallison.fernandes@gmail.com>
2022-04-16 20:03:10 -0300

    refacto: move sut creation to a factory method

---
+++ src/presentation/controllers/login/login.spec.ts
@@ -0,0 +1,33 @@
+import { MissingParamError } from '../../errors'
+import { badRequest } from '../../helpers/http-helper'
+import { LoginController } from './login'
+interface SutTypes {
+  sut: LoginController
+}
+const makeSut = (): SutTypes => {
+  const sut = new LoginController()
+  return { sut }
+}
+describe('Login controller ', () => {
+  test('Should Return 400 if no email is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
+  })
+
+  test('Should Return 400 if no password is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
+  })
+})
---
+++ src/presentation/controllers/login/login.ts
@@ -0,0 +1,18 @@
+import { MissingParamError } from '../../errors'
+import { badRequest } from '../../helpers/http-helper'
+import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+
+export class LoginController implements Controller {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const requiredFields = ['email', 'password']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
+    }
+    return {
+      statusCode: 200,
+      body: {}
+    }
+  }
+}
