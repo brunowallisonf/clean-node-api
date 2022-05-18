9491f74a5833e9eca6a3020f4feb4edd733f4530
Bruno Wallison <brunowallison.fernandes@gmail.com>
2022-05-18 09:02:30 -0300

    test: ensure encrypt call jwt correctly

---
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -0,0 +1,12 @@
+import jwt from 'jsonwebtoken'
+import { JwtAdapter } from './jwt-adapter.'
+
+describe('Jwt Adapter', function () {
+  test('Should call sign with correct values', async () => {
+    const sut = new JwtAdapter('secret')
+    const signSpy = jest.spyOn(jwt, 'sign')
+
+    await sut.encrypt('any_value')
+    expect(signSpy).toHaveBeenCalledWith({ id: 'any_value' }, 'secret')
+  })
+})
